a4728f0a00b8cc4eb5fbb8726596f901319f5a07
Aleksander210 <aleksandar210@abv.bg>
2020-03-25 17:38:37 +0200

    added task related to Streams and Files

--- SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Files_Streams.java
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Files_Streams.java
@@ -2,14 +2,42 @@ package advanced_Softuni;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Files_Streams {
-	static String path = "D:\\Eclispe workspace for java\\textFilesStreamsFiles";
+	static String path = "D:\\textFilesStreamsFiles";
 	public static void main(String[] args) {
-		displayFileInformation();
+		//displayFileInformation();
+		//transferDataBetween();
 		
 	}
+	
+	
+	static void transferDataBetween() {
+		String readFrom = "\\readFile.txt";
+		String writeFile = "\\writeFile.txt";
+		
+		try(FileInputStream fis = new FileInputStream(path+readFrom);
+				FileOutputStream fos = new FileOutputStream(path+writeFile)){
+			
+			int read = fis.read();
+			while(read>-1) {
+				fos.write(read);
+				fis.read();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 	
 	
 	static void displayFileInformation() {

53f1a41c9d9097de224d8a6e65633a7bb0aa0d1a
Aleksander210 <aleksandar210@abv.bg>
2020-03-25 23:27:14 +0200

    added a new task related to Streams and Files

--- SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Files_Streams.java
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Files_Streams.java
@@ -1,18 +1,57 @@
 package advanced_Softuni;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+
+
 
 public class Files_Streams {
-	static String path = "D:\\textFilesStreamsFiles";
+	static String path = "D:\\FileFolder";
 	public static void main(String[] args) {
 		//displayFileInformation();
 		//transferDataBetween();
-		
+		//
+		//
+		//
+	//	transferModifiedData();
 	}
 	
+	
+	static void transferModifiedData() {
+		String readFrom = "\\Read.txt";
+		String writeFile = "\\Put.txt";
+		//java.nio.file.Path getFileData = Paths.get(path+readFrom);
+		//java.nio.file.Path putFileData = Paths.get(path+writeFile);
+		
+		try (BufferedReader  bfr = new BufferedReader(new FileReader(path+readFrom));
+				PrintWriter pw = new PrintWriter(new FileWriter(path+writeFile))){
+			
+			String readLine = bfr.readLine();
+			while(readLine!=null) {
+				pw.println(readLine);
+				readLine = bfr.readLine();
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//------------------------------------------------------------------------------
 	
 	static void transferDataBetween() {
 		String readFrom = "\\readFile.txt";
@@ -37,7 +76,7 @@ public class Files_Streams {
 	}
 	
 	
-	
+	//------------------------------------------------------------------------------------
 	
 	
 	static void displayFileInformation() {

8b19ce68d22fcb8b21f22f98fa660888eeb9beec
Aleksander210 <aleksandar210@abv.bg>
2020-03-30 22:06:34 +0300

    updated task

--- SoftuniLabPractice/SoftuniLab/src/DefinignClasses_fodler_temp/Employee.java
+++ SoftuniLabPractice/SoftuniLab/src/DefinignClasses_fodler_temp/Employee.java
@@ -1,22 +1,68 @@
 package DefinignClasses_fodler_temp;
 
 public class Employee {
+	private static final String DEFAULT_MAIL = "n/a";
+	private static final int DEFAULT_AGE = -1;
 	private String name;
 	private int age;
 	private String positon;
 	private String department;
 	private double salary;
+	private String email;
 	
-	public Employee(String name,int age,double salary,String position,String department) {
+	public String getEmail() {
+		return email;
+	}
+
+
+	
+
+	public Employee(String name,double salary,String position,String department,String mail,int age) {
 		setName(name);
 		setAge(age);
 		setSalary(salary);
 		setPositon(positon);
 		setDepartment(department);
+		setEmail(mail);
+		
 	}
 	
 	
+	public Employee(String name,double salary,String position,String department,String temp) {
+		setName(name);
+		setPositon(position);
+		setDepartment(department);
+		setSalary(salary);
+		
+		try {
+			int probablyAge = Integer.parseInt(temp);
+			setAge(probablyAge);
+			setEmail(DEFAULT_MAIL);
+		}catch(NumberFormatException e) {
+			setAge(DEFAULT_AGE);
+			setEmail(temp);
+		}
+	}
+	
+	public Employee(String name,double salary,String position,String department) {
+		setName(name);
+		setPositon(position);
+		setDepartment(department);
+		setSalary(salary);
+		setEmail(DEFAULT_MAIL);
+		setAge(DEFAULT_AGE);
+		
+	}
+	
 
+	
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
 	public String getName() {
 		return name;
 	}

685d457698513a3f6ae439b63c9cb0cdc3cfe0cb
Aleksander210 <aleksandar210@abv.bg>
2020-03-30 17:28:26 +0300

    updated task

--- SoftuniLabPractice/SoftuniLab/src/DefinignClasses_fodler_temp/BankAccount.java
+++ SoftuniLabPractice/SoftuniLab/src/DefinignClasses_fodler_temp/BankAccount.java
@@ -1,17 +1,30 @@
 package DefinignClasses_fodler_temp;
 
 public class BankAccount {
-public static  int idCounter;
+public static  int idCounter=0;
 public static  double interestRate = 0.2d;
 private int id;
 private double balance;
 
 
+public BankAccount() {
+	idCounter++;
+	this.id = idCounter;
+	this.balance=0;
+}
 
-//static method for changing intrest rate
+//static method for changing interest rate
 public static void setInterestRate(double interest) {
 	interestRate = interest;
 }
 
+//method to check interest rate for years entered
+public double getInterest(int years) {
+	return interestRate*years*this.balance;
+}
+
+public void deposit(double sum) {
+	this.balance+=sum;
+}
 
 }

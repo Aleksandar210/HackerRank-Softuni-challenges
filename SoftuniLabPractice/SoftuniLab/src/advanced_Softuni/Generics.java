68029c29000a98301a20225fe9d44aae202a75f1
Aleksander210 <aleksandar210@abv.bg>
2020-04-03 15:55:23 +0300

    updated new task

--- SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Generics.java
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni/Generics.java
@@ -2,8 +2,12 @@ package advanced_Softuni;
 
 public class Generics {
 	public static void main(String[] args) {
-		
+		//jarTask();
 	}
 	
+	
+	static void jarTask() {
+		//specify Type  Jar<> jar = new Jar<>();
+	}
 
 }

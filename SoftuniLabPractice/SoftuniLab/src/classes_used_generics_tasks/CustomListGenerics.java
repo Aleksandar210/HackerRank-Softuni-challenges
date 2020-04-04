8349feaa66aad8fbf4f065a2ec8b244fe6a8b924
Aleksander210 <aleksandar210@abv.bg>
2020-04-04 16:30:58 +0300

    updated new added task

--- SoftuniLabPractice/SoftuniLab/src/classes_used_generics_tasks/CustomListGenerics.java
+++ SoftuniLabPractice/SoftuniLab/src/classes_used_generics_tasks/CustomListGenerics.java
@@ -1,8 +1,11 @@
 package classes_used_generics_tasks;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
 public class CustomListGenerics<E extends Comparable<E>> implements List<E> {
 private List<E> list;
 
@@ -75,7 +78,19 @@ public CustomListGenerics(List<E> list) {
 	
 	}
 	
+	public int countGreatherThan(E element) {
+		List<E> currentStreamList = new ArrayList<E>(this.list);
+		this.list.stream().filter(e -> e.compareTo(element)>1).collect(Collectors.toList());
+		return currentStreamList.size();
+	}
 	
+	public E getMin() {
+		return Collections.min(this.list);
+	}
+	
+	public E getMax() {
+		return Collections.max(this.list);
+	}
 	
 
 	

1f6bbb7d41b41ccb125a633c118f71b94031e8e5
Aleksander210 <aleksandar210@abv.bg>
2020-04-04 16:50:13 +0300

    updated new added task

---
+++ SoftuniLabPractice/SoftuniLab/src/classes_used_generics_tasks/Tuple.java
@@ -0,0 +1,14 @@
+package classes_used_generics_tasks;
+
+public class Tuple<E,T> {
+ E element;
+ T element1;
+ 
+ public Tuple(E element, T element1) {
+	 this.element = element;
+	 this.element1 = element1;
+ }
+ 
+ 
+ 
+}

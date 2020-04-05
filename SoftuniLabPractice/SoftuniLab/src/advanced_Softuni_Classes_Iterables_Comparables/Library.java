17b08381c2d4cf54ed0a323be9a1c51b167ca115
Aleksander210 <aleksandar210@abv.bg>
2020-04-05 14:37:06 +0300

    updated and added new class to task

--- SoftuniLabPractice/SoftuniLab/src/advanced_Softuni_Classes_Iterables_Comparables/Library.java
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni_Classes_Iterables_Comparables/Library.java
@@ -1,9 +1,57 @@
 package advanced_Softuni_Classes_Iterables_Comparables;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Library implements Iterable<Book> {
 	List<Book> books;
+	
+	public Library() {
+		this.books = new ArrayList<Book>();
+	}
+	
+	public void addBook(Book currentBookAdd) {
+		this.books.add(currentBookAdd);
+	}
+
+	@Override
+	public Iterator<Book> iterator() {
+		
+		return new LibIterator(books);
+	}
+	
+	
+	
+
+	private class LibIterator implements Iterator<Book>{
+		private int indexOfBook;
+		private List<Book> listWithBooks;
+		
+		public LibIterator(List<Book> listWithBooks) {
+			indexOfBook = 0;
+			this.listWithBooks=listWithBooks;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if(indexOfBook<listWithBooks.size()) {
+				return true;
+			}else {
+			return false;	
+			}
+			 
+		}
+
+		@Override
+		public Book next() {
+			Book currentBook = listWithBooks.get(indexOfBook);
+			indexOfBook++;
+			return currentBook;
+			
+		}
+		
+	}
 	
 
 }

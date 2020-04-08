f8761fa4ec00f632e022f0017be027687275844d
Aleksander210 <aleksandar210@abv.bg>
2020-04-08 16:51:41 +0300

    updated new added task

--- SoftuniLabPractice/SoftuniLab/src/advanced_Softuni_Classes_Iterables_Comparables/Guild.java
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni_Classes_Iterables_Comparables/Guild.java
@@ -1,15 +1,21 @@
 package advanced_Softuni_Classes_Iterables_Comparables;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Guild implements Iterable<Player>{
 	private static final int DEF_SIZE =0;
 private Player[] players;
 private String name;
 private Map<String,Integer> searchByNames;
+private Set<String> clazzezInGuild = new HashSet<String>();
 private int size;
 
 
@@ -21,6 +27,7 @@ public Guild(String name, int capacity) {
 
 public void addPlayer(Player currentPlayer) {
 	currentPlayer.setRank("Trial");
+	this.clazzezInGuild.add(currentPlayer.getClazz());
 	if(getSize()==getCapacity()) {
 		System.out.println("No more room");
 	}else {
@@ -49,6 +56,23 @@ public void promotePlayer(String playerName) {
 
 public void demotePlayer(String playerName) {
 	this.players[searchByNames.get(playerName)].setRank("TRIAL");
+}
+
+public void kickPlayerByClass(String clazz) {
+	if(this.clazzezInGuild.contains(clazz)) {
+		List<Player> currentPlayers = new ArrayList<Player>();
+		currentPlayers = Arrays.stream(this.players)
+				.filter(e ->  !e.getClazz().equals(clazz))
+				.collect(Collectors.toList());
+		Player[] temp = currentPlayers.toArray(new Player[currentPlayers.size()]);
+		this.players = temp;
+	}else {
+		
+	}
+}
+
+public int count() {
+	return getCapacity();
 }
 
 

db965093b3184f504a570dc93b20c91488bc7ad9
Aleksander210 <aleksandar210@abv.bg>
2020-04-12 15:38:11 +0300

    updated new adde d task

---
+++ SoftuniLabPractice/SoftuniLab/src/advanced_Softuni_Classes_Iterables_Comparables/Team.java
@@ -0,0 +1,67 @@
+package advanced_Softuni_Classes_Iterables_Comparables;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Team {
+private String name;
+private List<Person> first;
+private List<Person> reserve;
+private Map<String,Integer> older;
+private Map<String,Integer> younger;
+
+public Team(String name) {
+	setFirst();
+	setReserve();
+}
+
+
+
+private void setReserve() {
+	this.reserve = new LinkedList<Person>();
+	this.older = new LinkedHashMap<String,Integer>();
+}
+
+private void setFirst() {
+ this.first = new LinkedList<Person>();
+ younger = new LinkedHashMap<String,Integer>();
+}
+
+public List<Person> getFirst(){
+	return Collections.unmodifiableList(this.first);
+}
+
+public List<Person> getReserve(){
+	return Collections.unmodifiableList(this.reserve);
+}
+
+public void addFirst(Person current) {
+	this.first.add(current);
+}
+
+public void addReserve(Person current) {
+	this.reserve.add(current);
+	
+}
+
+@SuppressWarnings("unlikely-arg-type")
+public Person removeFirst(String name) {
+	Person removed = this.first.get(younger.remove(name));
+	this.first.remove(younger.remove(name));
+	
+	return removed;
+     
+}
+@SuppressWarnings("unlikely-arg-type")
+public void removeReserve(String name) {
+	this.reserve.remove(this.older.remove(name));
+}
+
+
+
+
+
+}

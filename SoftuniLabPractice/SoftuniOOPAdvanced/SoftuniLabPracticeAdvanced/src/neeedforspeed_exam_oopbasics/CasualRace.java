f46c2f367efa6f4fb4681ec3a448b3bc4c18e58b
Aleksander210 <aleksandar210@abv.bg>
2020-04-22 17:27:47 +0300

    updated new added task

--- SoftuniLabPractice/SoftuniOOPAdvanced/SoftuniLabPracticeAdvanced/src/neeedforspeed_exam_oopbasics/CasualRace.java
+++ SoftuniLabPractice/SoftuniOOPAdvanced/SoftuniLabPracticeAdvanced/src/neeedforspeed_exam_oopbasics/CasualRace.java
@@ -1,9 +1,25 @@
 package neeedforspeed_exam_oopbasics;
 
-public class CasualRace extends BaseRace {
+import java.util.Map;
+import java.util.TreeMap;
 
+public class CasualRace extends BaseRace {
+	private Map<Integer,Car> winners;
 	protected CasualRace(int length, String route, int prizePool) {
 		super(length, route, prizePool);
+		this.winners = new TreeMap<Integer,Car>();
+	}
+
+	@Override
+	public void getWinners() {
+		this.getParticipants().stream().map(e->{
+			this.winners.put(e.overallPerformance(), e);
+			return e;
+		});
+		int i=0;
+		for(Map.Entry<Integer, Car> entry:this.winners.entrySet()) {
+			System.out.println((i+1)+": "+entry.getValue());
+		}
 		
 	}
 

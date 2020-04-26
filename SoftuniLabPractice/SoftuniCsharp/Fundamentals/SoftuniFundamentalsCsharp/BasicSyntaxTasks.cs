71a275c97099a885eb8a49df8a5e0536fa3a55d8
Aleksander210 <aleksandar210@abv.bg>
2020-04-26 16:41:19 +0300

    updated new added task

---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentalsCsharp/BasicSyntaxTasks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftuniFundamentalsCsharp
+{
+    class BasicSyntaxTasks
+    {
+       
+        public void TimeCalculator()
+        {
+            int hours;
+            do
+            {
+                hours = int.Parse(Console.ReadLine());
+            }
+            while (hours<0 || hours>23);
+
+
+            int minutes;
+            do
+            {
+                minutes = int.Parse(Console.ReadLine());
+            }
+            while (minutes < 0 || minutes > 59);
+
+            int addMinutes = int.Parse(Console.ReadLine());
+
+            if(addMinutes +minutes >59)
+            {
+               minutes =  addMinutes + minutes - 60;
+                hours += 1;
+            }
+            else
+            {
+                minutes = addMinutes += minutes;
+            }
+
+            if(hours>23)
+            {
+                hours = hours - 24;
+            }
+
+            Console.WriteLine($"{hours:d2}:{minutes:d2}");
+
+
+            
+        }
+
+        public void DivisibleByThree()
+        {
+
+            for(int i = 1; i <= 100; i += 3)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentalsCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftuniFundamentalsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicSyntaxTasks currentTasks = new BasicSyntaxTasks();
+            currentTasks.TimeCalculator();
+            currentTasks.DivisibleByThree();
+
+           
+        }
+    }
+}

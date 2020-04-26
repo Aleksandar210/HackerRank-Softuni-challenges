48cfcbdf9c76c56ef1f3181cd5aed8375c3f68b0
Aleksander210 <aleksandar210@abv.bg>
2020-04-26 23:00:37 +0300

    updated new added task

--- SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentalsCsharp/DataTypes.cs
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentalsCsharp/DataTypes.cs
@@ -62,5 +62,21 @@ namespace SoftuniFundamentalsCsharp
             }
 
         }
+
+        public void PrimeChecker()
+        {
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 2; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}->{true}");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}->{false}");
+                }
+            }
+        }
     }
 }

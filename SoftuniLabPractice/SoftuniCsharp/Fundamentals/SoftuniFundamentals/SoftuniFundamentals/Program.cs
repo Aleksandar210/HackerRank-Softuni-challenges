1f37a9ae142ac0500aaccf0125c030710f11c47f
Aleksander210 <aleksandar210@abv.bg>
2020-04-27 20:31:06 +0300

    updated new added task

---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentals/SoftuniFundamentals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftuniFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = System.Text.RegularExpressions.Regex.Split(Console.ReadLine(),"\\s+");
+            
+            
+                Console.WriteLine(names[0]);
+            
+
+        }
+    }
+}

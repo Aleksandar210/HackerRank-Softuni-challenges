a1a031cb4c7430b3ef5ab4349c2f7701feb3179c
Aleksander210 <aleksandar210@abv.bg>
2020-04-27 17:47:50 +0300

    updated new added task

---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentalsCsharp/ArraysMethodsAndLists.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftuniFundamentalsCsharp
+{
+    class ArraysMethodsAndLists
+    {
+        public void EncrpytSortPrint()
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            int numberInputs = int.Parse(Console.ReadLine());
+            while(numberInputs-- > 0)
+            {
+                string enterWord = Console.ReadLine();
+                words.Add(enterWord, GetSumOfVolews(enterWord));
+
+
+            }
+
+            
+
+            foreach (KeyValuePair<string, int> item in words.OrderBy(key => key.Value))
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+
+        private int GetSumOfVolews(string word)
+        {
+            int sum = 0;
+            char[] c =  word.ToCharArray();
+            for(int i = 0; i < c.Length; i++)
+            {
+                switch (c[i].ToString().ToLower())
+                {
+                    case "a":
+                       
+                    case "e":
+                        
+                    case "i":
+                       
+                    case "o":
+
+                    case "u":
+                        sum += (int)c[i];
+                        break;
+                }
+            }
+            return sum;
+
+        }
+
+
+    }
+}

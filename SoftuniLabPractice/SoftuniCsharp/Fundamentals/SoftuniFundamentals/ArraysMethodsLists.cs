c29046861c81e284d231a70ecdb74240f2bce758
Aleksander210 <aleksandar210@abv.bg>
2020-04-28 21:58:04 +0300

    updated new added task

---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentals/ArraysMethodsLists.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SoftuniFundamentals
+{
+    class ArraysMethodsLists
+    {
+        public void Rotation()
+        {
+            string[] elements = Regex.Split(Console.ReadLine(), "\\s+");
+            int numberRotations = int.Parse(Console.ReadLine());
+            while(numberRotations-- > 0)
+            {
+                string firstElement = elements[0];
+                for(int i = 1; i < elements.Length; i++)
+                {
+                    elements[i - 1] = elements[i];
+                }
+                elements[elements.Length - 1] = firstElement;
+            }
+
+            foreach(string item in elements)
+            {
+                Console.Write(item+" ");
+            }
+
+        }
+
+        public void EqualSums()
+        {
+            int[] numbers = Regex.Split(Console.ReadLine(), "\\s+").Select(e => int.Parse(e)).ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            for(int i = 0; i < numbers.Length-1; i++)
+            {
+                if (i == 0)
+                {
+                    for (int l = i + 1; l < numbers.Length; l++)
+                    {
+                        rightSum += numbers[l];
+                    }
+
+                }
+                else
+                {
+                    for(int l = i + 1; l < numbers.Length; l++)
+                    {
+                        rightSum += numbers[l];
+                    }
+
+                    for(int k = 0; k < i; k++)
+                    {
+                        leftSum += numbers[k];
+                    }
+                }
+
+
+                if (leftSum == rightSum)
+                {
+                    Console.Write(numbers[i]+" ");
+                }
+            }
+
+           
+
+        }
+
+        public void KaminoFactory()
+        {
+            List<string> sequences = new List<string>();
+            string enterSequence;
+            do
+            {
+                enterSequence = Console.ReadLine();
+                if(enterSequence.Equals("Clone Them!", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                else
+                {
+                    sequences.Add(Regex.Split(enterSequence, "\\s+").ToString());
+                }
+
+
+            }
+            while (!enterSequence.Equals("Clone Them!", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ SoftuniLabPractice/SoftuniCsharp/Fundamentals/SoftuniFundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoftuniFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraysMethodsLists currentTasks = new ArraysMethodsLists();
+            //currentTasks.Rotation();
+            //currentTasks.EqualSums();
+
+        }
+    }
+}

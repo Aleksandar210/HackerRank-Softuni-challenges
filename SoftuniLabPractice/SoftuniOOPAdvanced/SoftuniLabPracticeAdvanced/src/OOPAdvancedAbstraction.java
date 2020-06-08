d9fb5ad385f533e16876c7cae320f628343c39f3
Aleksander210 <aleksandar210@abv.bg>
2020-06-08 16:56:46 +0300

    updated new added task

--- SoftuniLabPractice/SoftuniOOPAdvanced/SoftuniLabPracticeAdvanced/src/OOPAdvancedAbstraction.java
+++ SoftuniLabPractice/SoftuniOOPAdvanced/SoftuniLabPracticeAdvanced/src/OOPAdvancedAbstraction.java
@@ -1,10 +1,15 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class OOPAdvancedAbstraction {
 	static Scanner scan  = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+		HashMap<Integer,String> currentMap = new HashMap<Integer,String>();
+		for(Map.Entry<Integer, String> currentEntry:currentMap.entrySet()) {
+			
+		}
 		
 	}
 	

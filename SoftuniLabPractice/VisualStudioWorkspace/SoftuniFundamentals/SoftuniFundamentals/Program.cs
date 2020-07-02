3d36e96e9a27716c0773e9783af8aaca908d1266
Aleksander210 <aleksandar210@abv.bg>
2020-07-02 16:53:10 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Program.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Program.cs
@@ -259,7 +259,7 @@ namespace SoftuniFundamentals
             //  Ranking();
 
             //ReadOddLines();
-            AddNumericsToFileLines();
+            //AddNumericsToFileLines();
             
             
         }

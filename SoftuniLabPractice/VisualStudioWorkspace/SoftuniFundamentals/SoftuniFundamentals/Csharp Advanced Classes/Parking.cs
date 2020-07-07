d860d334386c6477de35b5c0cd9a251cc9351b2d
Aleksander210 <aleksandar210@abv.bg>
2020-07-07 23:19:37 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/Parking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes
+{
+    class Parking
+    {
+        //adding fields
+        private Dictionary<string, Car> currentParking;
+        private int capacity;
+
+        public Parking()
+        {
+            this.currentParking = new Dictionary<string, Car>();
+            this.capacity = 0;
+        }
+
+        public Parking(int capacity)
+        {
+
+        }
+
+        //adding prperties
+        public int Capacity
+        {
+            get { return this.capacity; }
+
+            private set 
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Space cannot be 0 or below that");
+                }
+                else
+                {
+                    this.capacity = value;
+                }
+            }
+        }
+    }
+}

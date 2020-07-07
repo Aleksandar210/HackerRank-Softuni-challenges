841b92e68d6e96fc515141845b10573669ff5e39
Aleksander210 <aleksandar210@abv.bg>
2020-07-07 22:54:00 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes
+{
+   public class Car
+    {
+        //adding fields
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string make,string model,int year,double fuelQuantity,double fuelConsumption)
+        {
+
+        }
+
+        //adding properties with validation
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year
+        {
+             get { return this.year; }
+
+            private set
+            { 
+                if(value<=0 || value<=1950)
+                {
+                    throw new ArgumentException("yer cannot be les or equal to 0 or 1960");
+                }
+            }
+        }
+
+    }
+}

6db52aa9963cb95cc964924aa24598ceaf9a8291
Aleksander210 <aleksandar210@abv.bg>
2020-07-11 23:39:09 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/Guild.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes
+{
+    class Guild
+    {
+        //adding fields;
+        private string name;
+        private int capacity;
+        
+
+        private Player[] roster;
+
+        //adding constructors
+        public Guild(string name,int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new Player[this.Capacity];
+
+        }
+
+        //adding properties
+
+        public string Name 
+        {
+            private set
+            {
+                if(value!=null)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Name or Null");
+                }
+            }
+
+            get { return this.name; }
+        }
+
+        public int Capacity
+        {
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Invaid Capacity cannot be 0 or below");
+                }
+                else
+                {
+                    this.capacity = value;
+                }
+            }
+
+            get { return this.capacity; }
+        }
+
+
+        //adding behaviour
+        
+    }
+}

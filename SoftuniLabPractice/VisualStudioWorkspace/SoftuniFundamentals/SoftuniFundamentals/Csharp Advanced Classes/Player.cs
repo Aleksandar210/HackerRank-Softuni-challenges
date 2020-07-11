1f4291d31037433fec6dbecdba8df44aadbcad42
Aleksander210 <aleksandar210@abv.bg>
2020-07-11 23:09:33 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/Player.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/Player.cs
@@ -10,6 +10,36 @@ namespace SoftuniFundamentals.Csharp_Advanced_Classes
         private const string DefaultRank = "Trial";
         private const string DefaultDescription = "n/a";
 
+        //adding fields
+        private string name;
+        private string playerClass;
+        private string description;
+        private string playerRank;
+
+        //adding constructors
+
+        public Player(string name, string playerClass)
+        {
+
+        }
+
+        //adding properties
+        public string Name
+        {
+            private set
+            {
+                if (value != null)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Player Name");
+                }
+            }
+
+            get { return this.name; }
+        }
 
     }
 }

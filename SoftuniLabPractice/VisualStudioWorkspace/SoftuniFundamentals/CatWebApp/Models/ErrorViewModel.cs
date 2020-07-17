d4ae95a29755efac08a66ffdc359befc46beebbd
Aleksander210 <aleksandar210@abv.bg>
2020-07-17 13:36:22 +0300

    added new projet

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/CatWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CatWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

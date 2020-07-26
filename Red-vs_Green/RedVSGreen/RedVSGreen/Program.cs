a39954ecd0383eda10c118485c494d16fa13fabf
Aleksander210 <aleksandar210@abv.bg>
2020-07-27 02:36:03 +0300

    updated new added tas

---
+++ Red-vs_Green/RedVSGreen/RedVSGreen/Program.cs
@@ -0,0 +1,15 @@
+﻿using RedVSGreen.Engine;
+using System;
+
+namespace RedVSGreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Startup currentGame = new Startup(3,3);
+        }
+
+
+    }
+}

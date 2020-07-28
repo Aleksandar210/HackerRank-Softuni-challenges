9d8a44c57385b9dac5fdbdd3ede923f8cdc959ca
Aleksander210 <aleksandar210@abv.bg>
2020-07-28 18:46:41 +0300

    updated new added task

--- Red-vs-Green/Red-vs-Green/Matrix.cs
+++ Red-vs-Green/Red-vs-Green/Matrix.cs
@@ -6,5 +6,31 @@ namespace Red_vs_Green
 {
     class Matrix
     {
+
+        //fields
+        Cell[,] field;
+
+        //constructors
+        public Matrix(int rows, int cols)
+        {
+
+        }
+
+        //behaviour
+        private void CreateField(int rows, int cols)
+        {
+            if(cols<=1000 && rows <= cols)
+            {
+                this.field = new Cell[rows, cols];
+            }
+        }
+
+        private void FillMatrixOnStart()
+        {
+            for(int i=0;i<this.field.GetLength(0);i++)
+            {
+
+            }
+        }
     }
 }

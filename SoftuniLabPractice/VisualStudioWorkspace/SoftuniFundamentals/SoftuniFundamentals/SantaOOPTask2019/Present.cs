171353eff7460a0538c7eccf8c554d826f504fe7
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 02:22:50 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Present.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Present.cs
@@ -12,23 +12,45 @@ namespace SoftuniFundamentals.SantaOOPTask2019
 
         public Present(string name, int energy)
         {
-
+            this.isDone = false;
+            this.Name = name;
+            this.EnergyRequired = energy;
+            
         }
 
         public string Name
         {
-
+            private set
+            {
+                if(String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Present name cannot be null or empty.");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+            get { return this.name; }
         }
 
         public int EnergyRequired
         {
+            private set
+            {
+                this.energyRequired = value;
+                if(value<0)
+                {
+                    this.energyRequired = 0;
+                    this.isDone = true; 
+                }
+            }
 
+            get { return this.energyRequired; }
         }
 
-        public bool IsDone
-        {
-
-        }
+        public bool IsDone => this.isDone;
+     
 
         public void GetCrafted()
         {

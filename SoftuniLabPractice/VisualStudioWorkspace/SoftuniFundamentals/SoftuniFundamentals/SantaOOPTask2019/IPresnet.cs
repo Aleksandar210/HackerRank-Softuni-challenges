9e632204c4bef81e416c75ea7e400cf2bab7d511
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 02:15:47 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IPresnet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    public interface IPresnet
+    {
+        string Name { get; }
+        int EnergyRequired { get; }
+        bool IsDone { get; }
+        void GetCrafted();
+    }
+}

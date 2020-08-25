62e799964d75bd9ac43f1935b1c65cc69a1da2b6
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 02:05:15 +0300

    updated new added taskgit push

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/SleepyDwarf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    class SleepyDwarf : Dwarf
+    {
+        public SleepyDwarf(string name):base(name,50)
+        {
+
+        }
+        public override void Work()
+        {
+            this.Energy -= 15;
+        }
+    }
+}

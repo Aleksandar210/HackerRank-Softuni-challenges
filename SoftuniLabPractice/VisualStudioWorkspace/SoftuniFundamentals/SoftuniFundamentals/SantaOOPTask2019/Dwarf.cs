85a0dbf71d24ddb6ef22f3d58d8df5fbf391222b
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 01:48:57 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Dwarf.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Dwarf.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace SoftuniFundamentals.SantaOOPTask2019
 {
-   public abstract class Dwarf
+    public abstract class Dwarf : IDwarf
     {
+        protected string name;
+        protected int energy;
+        protected Dictionary<string, Instrument> instruments;
+        public string Name 
+        {
+           private set
+            {
+
+            }
+            get { return this.name; }
+        }
+        public int Energy
+        { 
+            private set
+            {
+                this.energy = value;
+
+                if(value<=0)
+                {
+                    this.energy = 0;
+                }
+                
+            }
+
+            get { return this.energy; }
+        }
+        public Dictionary<string, Instrument> Instruments
+        {
+            get => throw new NotImplementedException(); set => throw new NotImplementedException();
+        }
     }
 }
--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IDwarf.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IDwarf.cs
@@ -7,8 +7,8 @@ namespace SoftuniFundamentals.SantaOOPTask2019
    public interface IDwarf
     {
         
-        string Name { get; set; }
-        int Energy { get; set; }
-        Dictionary<string, Instrument> Instruments { get; set;}
+        string Name { get;  }
+        int Energy { get; }
+        Dictionary<string, Instrument> Instruments { get;}
     }
 }

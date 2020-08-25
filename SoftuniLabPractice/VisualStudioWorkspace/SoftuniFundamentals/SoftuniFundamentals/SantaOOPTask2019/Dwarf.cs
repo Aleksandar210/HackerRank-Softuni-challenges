fd191e8f3764d07aa4b2fd7c55b458e20dc0ed9d
Aleksandar210 <aleksandar210@abv.bg>
2020-08-25 19:33:11 +0300

    added a new class

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Dwarf.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+   public abstract class Dwarf
+    {
+    }
+}
---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IDwarf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+   public interface IDwarf
+    {
+        
+        string Name { get; set; }
+        int Energy { get; set; }
+        Dictionary<string, Instrument> Instruments { get; set;}
+    }
+}

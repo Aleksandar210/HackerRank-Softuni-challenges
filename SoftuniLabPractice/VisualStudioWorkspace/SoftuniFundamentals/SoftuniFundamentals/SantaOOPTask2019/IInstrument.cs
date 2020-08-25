17dab7451828d15776f6ca81e4eff35ae603fa16
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 02:09:40 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IInstrument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    interface IInstrument
+    {
+        int Power { get; }
+        bool IsBroken { get; }
+        void Use();
+    }
+}
--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Instrument.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Instrument.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace SoftuniFundamentals.SantaOOPTask2019
 {
-    public class Instrument
+    public class Instrument : IInstrument
     {
+        private int power;
+
+        public Instrument(int power)
+        {
+            
+        }
+        public int Power
+        {
+            private set
+            {
+                this.power = value;
+                if(value<=0)
+                {
+                    this.power = 0;
+                }
+            }
+            get { return this.power; }
+        }
+
+        public bool IsBroken => throw new NotImplementedException();
+
+        public void Use()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

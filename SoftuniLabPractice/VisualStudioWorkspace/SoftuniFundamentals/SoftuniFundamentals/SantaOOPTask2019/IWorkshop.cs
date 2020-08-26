c635049e0c4059669b56188732633d0b82f3ca8f
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 13:32:29 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IWorkshop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    interface IWorkshop
+    {
+        void Craft(IPresent present, IDwarf dwarf);
+    }
+}
---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/Workshop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    class Workshop : IWorkshop
+    {
+        public void Craft(IPresent present, IDwarf dwarf)
+        {
+           //calculate if everything will be ok.
+        }
+    }
+}

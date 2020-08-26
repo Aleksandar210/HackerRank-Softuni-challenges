01cb1723fd66a781b693229e26174ed5e26182dc
Aleksandar210 <aleksandar210@abv.bg>
2020-08-26 15:56:59 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SantaOOPTask2019/IController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SantaOOPTask2019
+{
+    public interface IController
+    {
+        void AddDwarf(string type,string name);
+        void AddInstrumentToDwarf(string name, int power);
+        void AddPresent(string name, int energy);
+        void CraftPresent(string presentName);
+        
+    }
+}

c64dc9db1b141807c2e8f0d36c82738be741ebfc
Aleksandar210 <aleksandar210@abv.bg>
2020-08-27 14:09:19 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/IVehicle.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/IVehicle.cs
@@ -6,5 +6,10 @@ namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
 {
     public interface IVehicle
     {
+        bool IsFull { get; }
+        bool IsEmpty { get; }
+        int Capacity { get; }
+         IReadOnlyCollection<Product> Trunk { get; }
+
     }
 }
--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/Vehicle.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/Vehicle.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
 {
-    public abstract class Vehicle
+    public abstract class Vehicle : IVehicle
     {
+        public bool IsFull => throw new NotImplementedException();
+
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public int Capacity => throw new NotImplementedException();
+
+        public IReadOnlyCollection<Product> Trunk => throw new NotImplementedException();
     }
 }

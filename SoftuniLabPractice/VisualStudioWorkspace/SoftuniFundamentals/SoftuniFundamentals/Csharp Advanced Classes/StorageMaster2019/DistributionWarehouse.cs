13670f4dde2180c94aacdabf2519c6a7daa49c28
Aleksandar210 <aleksandar210@abv.bg>
2020-08-27 21:38:32 +0300

    updated new added task

--- SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/DistributionWarehouse.cs
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/DistributionWarehouse.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
 {
-    public class AutomatedWarehouse : Storage
+    public class DistributionWarehouse : Storage
     {
-        public AutomatedWarehouse(string name) : base(name, 1, 2)
+        public DistributionWarehouse(string name):base(name,2,5)
         { }
     }
 }

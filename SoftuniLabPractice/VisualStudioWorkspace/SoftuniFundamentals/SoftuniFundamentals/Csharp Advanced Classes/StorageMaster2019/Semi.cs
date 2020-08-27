2d768ce74a2f197491ee73c27a3267f1d89f8d8d
Aleksandar210 <aleksandar210@abv.bg>
2020-08-27 18:49:34 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/Semi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
+{
+    public class Semi:Vehicle
+    {
+        public Semi() : base(10)
+        { }
+    }
+}

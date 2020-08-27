963b72d20a97e318d69422d85174e059091655cc
Aleksandar210 <aleksandar210@abv.bg>
2020-08-27 21:50:42 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/IStorageMaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
+{
+    public interface IStorageMaster
+    {
+        string AddProduct(string type, double price);
+        string RegisterStorage(string type, string name);
+        string SelectVehicle(string storageName, int garageSlot);
+        string LoadVehicle(IEnumerable<Product>products);
+        string SendVehicleTo(string sourceName, int sourceGarageSlot, string destinationName);
+        string UnloadVehicle(string storageName, int garageSlot);
+        string GetStorageStatus(string storageName);
+        string GetSummary();
+
+    }
+}
---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/Csharp Advanced Classes/StorageMaster2019/StorageMaster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.Csharp_Advanced_Classes.StorageMaster2019
+{
+    public class StorageMaster : IStorageMaster
+    {
+        public string AddProduct(string type, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetStorageStatus(string storageName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSummary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LoadVehicle(IEnumerable<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RegisterStorage(string type, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SelectVehicle(string storageName, int garageSlot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SendVehicleTo(string sourceName, int sourceGarageSlot, string destinationName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UnloadVehicle(string storageName, int garageSlot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

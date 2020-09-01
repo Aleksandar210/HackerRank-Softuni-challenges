83dd045f911f6c5d39b23ebbfbe4e47e0164919e
Aleksandar210 <aleksandar210@abv.bg>
2020-09-01 22:07:02 +0300

    added new updated task

--- SoftuniJavaPracticeLab/src/Main.java
+++ SoftuniJavaPracticeLab/src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
-
+        //greenVsRedTask();
     }
 
     private static void greenVsRedTask(){

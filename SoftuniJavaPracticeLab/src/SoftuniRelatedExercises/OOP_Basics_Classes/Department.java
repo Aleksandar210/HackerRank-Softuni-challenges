bd9831f5005ce4bad9d7ccc4a4c8ddc0491b932b
Aleksandar210 <aleksandar210@abv.bg>
2020-09-03 21:46:36 +0300

    added new updated task

--- SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/Department.java
+++ SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/Department.java
@@ -1,6 +1,8 @@
 package SoftuniRelatedExercises.OOP_Basics_Classes;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Department {
     //static fields and constants
@@ -8,16 +10,35 @@ public class Department {
 
     //fields
     String name;
+    private double averageSalary;
+    private
+    List<Employe> currentEmployes;
+
 
     //constr
+    private Department(){
+        this.currentEmployes = new ArrayList<>();
+    }
     public Department(String name){
+        this();
         enterDepartmentNameInPool(name);
-
 
     }
 
     private static void enterDepartmentNameInPool(String name){
         currentDepartments.add(name.toLowerCase()); //do with lower for character difference
+    }
+
+    public double getAverageSalary(){
+        return this.averageSalary;
+    }
+
+    private void setAverageSalary(){
+        String doubleAsText = "12345.6789";
+        double number = Double.parseDouble(doubleAsText);
+        int decimal = Integer.parseInt(doubleAsText.split("\\.")[0]);
+        int fractional = Integer.parseInt(doubleAsText.split("\\.")[1]);
+
     }
 
 }

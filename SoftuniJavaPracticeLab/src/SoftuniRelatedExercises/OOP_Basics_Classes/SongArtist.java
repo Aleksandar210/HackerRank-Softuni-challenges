d26100a8af7882e7dc47cc601ef9b50483bfc9af
Aleksandar210 <aleksandar210@abv.bg>
2020-09-08 08:27:43 +0300

    added new updated task

--- SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/SongArtist.java
+++ SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/SongArtist.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.IllformedLocaleException;
 
 public class SongArtist {
+    //resources
+    private StringBuilder sb;
 
     //fields
     private String name;
@@ -13,6 +15,7 @@ public class SongArtist {
 
     public SongArtist(String name){
     this.currentSongs = new HashMap<>();
+    this.sb = new StringBuilder();
     this.setName(name);
     }
 
@@ -33,9 +36,7 @@ public class SongArtist {
     }
 
     public String getTotalTimeOfSongs(){
-        //return this.totalTimeOfSongs;
-        return null;
-
+       return String.format("%d:%d:%d",this.totalTimeOfSongs[0],this.totalTimeOfSongs[1],this.totalTimeOfSongs[2]);
     }
 
     private boolean validateSongName(String songName){
@@ -56,25 +57,59 @@ public class SongArtist {
         int hour = timeParts[0];
         int minutes = timeParts[1];
         int seconds = timeParts[2];
+        if(hour<0 || (minutes<0 || minutes>59) || (seconds<0|| seconds>59)){
+            throw new IllegalArgumentException("Illegal length state.");
+        }else{
+        return true;
+        }
 
-        //d valdiation here
+    }
 
-        return false;
-
+    private int[] getTimePartsOfDuration(String duration){
+        int[] timeParts = Arrays.stream(duration.split(":")).mapToInt(Integer::parseInt).toArray();
+        int hour = timeParts[0];
+        int minutes = timeParts[1];
+        int seconds = timeParts[2];
+        return timeParts;
     }
 
     public void addSong(String songName, String duration){
     if(this.validateSongName(songName)){
+    if(this.validateDuration(duration)){
+        this.addToTotalTime(this.getTimePartsOfDuration(duration));
+        if(this.currentSongs.containsKey(songName)){
+            throw new IllegalArgumentException("Song already in playlist.");
+        }else{
+            this.currentSongs.put(songName,duration);
+        }
 
+    }
     }else{
         throw new IllegalArgumentException("Illegal song state.");
     }
-
-
+    this.sb.append(songName+" - "+duration+System.lineSeparator());
     }
 
     private void addToTotalTime(int...hourMinuteSecond){
+        if(hourMinuteSecond[2]+this.totalTimeOfSongs[2]>59){
+            this.totalTimeOfSongs[2]=hourMinuteSecond[2]-this.totalTimeOfSongs[2];
+            this.addMinutes(1);
+        }
+        this.addMinutes(hourMinuteSecond[1]);
+    this.totalTimeOfSongs[0]+=hourMinuteSecond[0];
+    }
 
+    private void addMinutes(int minutesToAdd){
+        this.totalTimeOfSongs[1]+=minutesToAdd;
+        if(this.totalTimeOfSongs[1]>59){
+            this.totalTimeOfSongs[1]-=60;       //the most simple way got to improve it
+            this.totalTimeOfSongs[0]+=1;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return null;
     }
 
 

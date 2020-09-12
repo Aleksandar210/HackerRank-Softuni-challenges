6af79bc60d6741f3d1cdc4eb6ca36a59672025db
Aleksandar210 <aleksandar210@abv.bg>
2020-09-12 17:06:11 +0300

    added new updated task

--- SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/Younger_Scrolls/YoungerSoulsController.java
+++ SoftuniJavaPracticeLab/src/SoftuniRelatedExercises/OOP_Basics_Classes/Younger_Scrolls/YoungerSoulsController.java
@@ -1,11 +1,25 @@
 package SoftuniRelatedExercises.OOP_Basics_Classes.Younger_Scrolls;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class YoungerSoulsController implements IRunnable {
 
+private Scanner scan;
+private HashMap<String,Province> provinces;
+private
+
+public YoungerSoulsController(){
+    this.scan = new Scanner(System.in);
+}
 
     @Override
     public void run() {
+        String enterCommand = this.scan.nextLine();
+        String[] commandParts;
+        while(!enterCommand.equalsIgnoreCase("end")){
 
+        }
     }
 
 

174d15d0de0781eddc1fd2b0740d65ed3c326d2a
Aleksandar210 <aleksandar210@abv.bg>
2020-09-28 02:54:23 +0300

    updated new added task

--- SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/FootballTeam.java
+++ SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/FootballTeam.java
@@ -3,11 +3,40 @@ package OOP_Advanced_Classes.Encapsulation;
 import java.util.HashMap;
 
 public class FootballTeam {
+    private String name;
     private HashMap<String,FootballPlayer> currentPlayers;
     private double teamRating;
 
     public FootballTeam(String name){
+    this.currentPlayers = new HashMap<>();
+    }
+
+    public String getName(){return this.name;}
+    private void setName(String name){
+        if(name!=null && !name.trim().isEmpty()){
+            this.name = name;
+        }else{
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+    }
+
+    public void addPlayer(FootballPlayer player){
+        if(this.currentPlayers.containsKey(player.getName())){
+            throw new IllegalArgumentException(String.format("Player %s already in team",player.getName()));
+        }else{
+            this.currentPlayers.put(player.getName(),player);
+        }
+    }
+
+    public void removePlayer(String name){
+        if(this.currentPlayers.containsKey(name)){
+            this.currentPlayers.remove(name);
+        }else{
+            throw new IllegalArgumentException(String.format("Player %s is not in %s",name,this.))
+        }
 
     }
 
 }
+
+

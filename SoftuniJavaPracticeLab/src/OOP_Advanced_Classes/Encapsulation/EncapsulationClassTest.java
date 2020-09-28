299aa99b440c461ff5b1646bf5f74a2933d4117b
Aleksandar210 <aleksandar210@abv.bg>
2020-09-28 03:09:30 +0300

    updated new added task

--- SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/EncapsulationClassTest.java
+++ SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/EncapsulationClassTest.java
@@ -61,8 +61,11 @@ private Scanner scan;
         case "Add":
             if(this.doesTeamExist(command[1],teams)){
                 try{
+                    //entering stats in array for more clear look
                     int[] stats = {Integer.valueOf(command[3]),Integer.valueOf(command[4])
                     ,Integer.valueOf(command[5]),Integer.valueOf(command[6]),Integer.valueOf(command[7])};
+
+                    //adding and creating a new player object if there is invalid data an exception will appear
                     teams.get(command[1]).addPlayer(new FootballPlayer(command[2],stats[0],stats[1],stats[2]
                     ,stats[3],stats[4]));
                 }catch(IllegalArgumentException exc){

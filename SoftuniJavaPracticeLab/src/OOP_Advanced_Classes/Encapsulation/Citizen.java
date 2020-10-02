6a4fd3a92580e6d987e46d035a7c519f79635ed9
Aleksandar210 <aleksandar210@abv.bg>
2020-10-02 03:06:23 +0300

    updated new added task

--- SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/Citizen.java
+++ SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/Citizen.java
@@ -1,4 +1,14 @@
 package OOP_Advanced_Classes.Encapsulation;
 
-public class Citizen {
+public class Citizen extends FoodShortagePerson {
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        super(name, age, id, birthDate);
+    }
+
+    @Override
+    public void buyFood() {
+        super.buyFood();
+        super.buyFood();
+    }
 }

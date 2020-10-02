d7ec5045e9ab5b35d084c8bdef3b17975d40388b
Aleksandar210 <aleksandar210@abv.bg>
2020-10-02 03:04:48 +0300

    updated new added task

---
+++ SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/FoodShortagePerson.java
@@ -0,0 +1,86 @@
+package OOP_Advanced_Classes.Encapsulation;
+
+import java.util.IllformedLocaleException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+abstract class FoodShortagePerson implements Buyer {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+    private int food;
+
+    public FoodShortagePerson(String name, int age, String id, String birthDate){
+    this.food = 0;
+    this.setName(name);
+    this.setAge(age);
+    this.setId(id);
+    this.setBirthDate(birthDate);
+    }
+
+    public String getBirthDate(){return this.birthDate;}
+
+  private void setBirthDate(String birthDate){
+        Pattern pattern = Pattern.compile("[[0-9]{2}|[0-9]{1}]/[[0-9]{2}|[0-9]{1}]/[0-9]+");
+        Matcher matcher = pattern.matcher(birthDate);
+        if(matcher.find()){
+            this.birthDate = birthDate;
+        }else{
+            throw new IllegalArgumentException("Invalid birthdate");
+        }
+  }
+
+  public String getId(){
+        return this.id;
+  }
+
+  private void setId(String id){
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(id);
+        if(matcher.find()){
+            this.id = id;
+        }else{
+            throw new IllegalArgumentException("INVALID ID");
+        }
+  }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name){
+        if(name!=null && !name.trim().isEmpty()){
+            this.name = name;
+        }else{
+            throw new IllegalArgumentException("Name cannot be empty or null");
+        }
+    }
+
+
+    public int getAge() {
+        return this.age;
+    }
+
+    private void setAge(int age){
+        if(age<=0){
+            throw new IllegalArgumentException("Invalid age");
+        }else{
+            this.age = age;
+        }
+    }
+
+
+
+
+    @Override
+    public void buyFood() {
+    this.food+=5;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+}
--- SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/Rebel.java
+++ SoftuniJavaPracticeLab/src/OOP_Advanced_Classes/Encapsulation/Rebel.java
@@ -1,75 +1,18 @@
 package OOP_Advanced_Classes.Encapsulation;
 
-import java.util.IllformedLocaleException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+public class Rebel extends FoodShortagePerson {
 
-abstract class FoodShortagePerson implements Buyer {
-    private String name;
-    private int age;
-    private String id;
-    private String birthDate;
-    private int food;
-
-    public FoodShortagePerson(String name, int age, String id, String birthDate){
-    this.food = 0;
-    this.setName(name);
-    this.setAge(age);
-    this.setId(id);
-    this.setBirthDate(birthDate);
+    public Rebel(String name, int age, String id, String birthDate) {
+        super(name, age, id, birthDate);
     }
 
-    public String getBirthDate(){return this.birthDate;}
+    @Override
+    public void buyFood() {
 
-  private void setBirthDate(String birthDate){
-        Pattern pattern = Pattern.compile("[[0-9]{2}|[0-9]{1}]/[[0-9]{2}|[0-9]{1}]/[0-9]+");
-        Matcher matcher = pattern.matcher(birthDate);
-        if(matcher.find()){
-            this.birthDate = birthDate;
-        }else{
-            throw new IllegalArgumentException("Invalid birthdate");
-        }
-  }
-
-  public String getId(){
-        return this.id;
-  }
-
-  private void setId(String id){
-        Pattern pattern = Pattern.compile("[0-9]+");
-        Matcher matcher = pattern.matcher(id);
-        if(matcher.find()){
-            this.id = id;
-        }else{
-            throw new IllegalArgumentException("INVALID ID");
-        }
-  }
-
-
-    public String getName() {
-        return this.name;
     }
 
-    private void setName(String name){
-        if(name!=null && !name.trim().isEmpty()){
-            this.name = name;
-        }else{
-            throw new IllegalArgumentException("Name cannot be empty or null");
-        }
+    @Override
+    public int getFood() {
+        return 0;
     }
-
-
-    public int getAge() {
-        return this.age;
-    }
-
-    private void setAge(int age){
-        if(age<=0){
-            throw new IllegalArgumentException("Invalid age");
-        }else{
-            this.age = age;
-        }
-    }
-
-
 }

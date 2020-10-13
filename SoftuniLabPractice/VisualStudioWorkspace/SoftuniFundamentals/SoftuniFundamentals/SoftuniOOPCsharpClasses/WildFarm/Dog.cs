8f7a20024890d4b23409b2274e4772fc0779f575
Aleksandar210 <aleksandar210@abv.bg>
2020-10-13 14:45:16 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SoftuniOOPCsharpClasses/WildFarm/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SoftuniOOPCsharpClasses.WildFarm
+{
+    public class Dog : Mammal
+    {
+        public Dog(string name, string livingRegion) : base(name, livingRegion)
+        {
+        }
+
+        public override void EatFood(Food food)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

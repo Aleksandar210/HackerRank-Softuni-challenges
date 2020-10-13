a25b7ea24c273c4618e2c3aad061e829b072d82a
Aleksandar210 <aleksandar210@abv.bg>
2020-10-13 14:54:59 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SoftuniOOPCsharpClasses/WildFarm/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SoftuniOOPCsharpClasses.WildFarm
+{
+    public class Cat : Feline
+    {
+        public Cat(string name, string livingRegion, string breed) : base(name, livingRegion, breed)
+        {
+        }
+
+        public override void EatFood(Food food)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

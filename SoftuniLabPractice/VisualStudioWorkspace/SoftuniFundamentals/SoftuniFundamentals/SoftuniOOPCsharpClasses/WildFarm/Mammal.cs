16c86170322d3e4ad8c8c45bc3e480ee2940c53d
Aleksandar210 <aleksandar210@abv.bg>
2020-10-13 14:43:52 +0300

    updated new added task

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SoftuniOOPCsharpClasses/WildFarm/Mammal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace SoftuniFundamentals.SoftuniOOPCsharpClasses.WildFarm
+{
+    public abstract class Mammal : Animal
+    {
+        private string livingRegion;
+        protected Mammal(string name,string livingRegion) : base(name)
+        {
+            this.LivingRegion = livingRegion;
+        }
+
+        public string LivingRegion
+        {
+            private set
+            {
+                if(!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                {
+                    this.livingRegion = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Region cannot be null or empty.");
+                }
+            }
+
+            get => this.livingRegion;
+        }
+    }
+}

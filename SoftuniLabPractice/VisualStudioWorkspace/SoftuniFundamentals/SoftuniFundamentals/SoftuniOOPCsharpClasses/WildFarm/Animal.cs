6f9869c759171189d282ff56a9c9b537052c3dc6
Aleksandar210 <aleksandar210@abv.bg>
2020-10-13 12:00:45 +0300

    added new class

---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SoftuniOOPCsharpClasses/WildFarm/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SoftuniOOPCsharpClasses.WildFarm
+{
+    public abstract class Animal
+    {
+        string name;
+        double weight;
+        int foodEaten;
+
+        protected Animal(string name)
+        {
+            this.Name = name;
+            this.Weight = 1;
+            this.foodEaten = 0;
+        }
+
+        public string Name
+        {
+            private set
+            {
+                if(!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+            }
+            get => this.name;
+        }
+
+        public double Weight
+        {
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Weight cannot be 0 or negative.");
+                }
+                else
+                {
+                    this.weight = value;
+                }
+            }
+
+            get => this.weight;
+        }
+        public int FoodEaten => this.foodEaten;
+
+        public abstract void EatFood(Food food);
+    }
+}
---
+++ SoftuniLabPractice/VisualStudioWorkspace/SoftuniFundamentals/SoftuniFundamentals/SoftuniOOPCsharpClasses/WildFarm/Food.cs
@@ -0,0 +1,34 @@
+﻿using MoreLinq.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftuniFundamentals.SoftuniOOPCsharpClasses.WildFarm
+{
+    public abstract class Food
+    {
+        int quantity;
+
+        protected Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+
+        public int Quantity
+        {
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Food quantity cannot be 0 or negative");
+                }
+                else
+                {
+                    this.quantity = value;
+                }
+            }
+
+            get => this.quantity;
+        }
+    }
+}

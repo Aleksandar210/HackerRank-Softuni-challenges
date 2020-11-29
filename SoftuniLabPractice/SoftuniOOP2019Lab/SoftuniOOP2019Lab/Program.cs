a85711ea2bb5c35c8a529eca2308b1fe5209a242
Aleksandar210 <aleksandar210@abv.bg>
2020-11-29 23:54:36 +0200

    updated new added test

---
+++ SoftuniLabPractice/SoftuniOOP2019Lab/SoftuniOOP2019Lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftuniOOP2019Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Small test for new project run
+            Console.Write("Enter numbers separated by space: ");
+            List<int> currentListWithNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(e => int.Parse(e))
+                .ToList();
+            int currentMinNumberInStream = int.MinValue;
+            int currentMaxNumberToDisplay;
+            for(int i=0;i<currentListWithNumbers.Count();i++)
+            {
+                if(currentListWithNumbers[i]>currentMinNumberInStream)
+                {
+                    currentMinNumberInStream = currentListWithNumbers[i];
+                }
+            }
+            Console.WriteLine(currentMinNumberInStream);
+                
+        }
+    }
+}
